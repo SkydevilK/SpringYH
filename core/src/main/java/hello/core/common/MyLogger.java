4d1f058ac3fb83b3ed66bb8f62852d2acbce6cb2
SkydevilK <skydevilk@gmail.com>
2022-06-05 23:25:30 +0900

    스코프와 프록시

--- core/src/main/java/hello/core/common/MyLogger.java
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// proxyMode 사용 시 CGLIB라는 바이트 코드를 조작하는 라이브러리를 사용하여 가짜 프록시 객체시를 생성한다.
 public class MyLogger {
 
     private String uuid;
