eb8eefdba9ccbd2612e85db5b32165e353e8a09d
SkydevilK <skydevilk@gmail.com>
2022-07-05 01:15:47 +0900

    스프링 부트 - 오류 페이지1

--- exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Controller
 @Slf4j
+@Controller
 public class ServletExceptionController {
 
     @GetMapping("/error-ex")
@@ -19,6 +19,11 @@ public class ServletExceptionController {
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
     }
 
     @GetMapping("/error-500")
